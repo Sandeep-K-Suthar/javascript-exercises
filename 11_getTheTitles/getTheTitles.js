9f6103a74f6a3275c2fcc6b2b207420109dc4c91
Sandeep Suthar <sandeep.khemani@outlook.com>
2025-03-22 17:22:24 +0500

    solve

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
+const getTheTitles = function (books) {
+    let title = [];
+    for (let index = 0; index < books.length; index++) {
+        title.push(books[index].title);
+
+    }
+    return title;
 
 };
+const books = [
+    {
+        title: 'Book1',
+        author: 'Lame',
+    },
+    {
+        title: 'Book2',
+        author: 'Lamo',
+    }
+]
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
